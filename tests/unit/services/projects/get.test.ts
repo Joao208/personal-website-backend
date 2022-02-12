e57fd859aba2ab7a2958cc85b504d477df4b0bc8
João Augusto <joao.augusto@foodzilla.com.br>
2022-02-11 22:59:44 -0300

    fix: fixing tests

--- tests/unit/services/projects/get.test.ts
+++ tests/unit/services/projects/get.test.ts
@@ -1,9 +1,15 @@
 import * as projects from "../../../../src/services/projects";
 
+jest.mock("../../../../src/config/prisma", () => ({
+  project: {
+    findMany: jest.fn().mockReturnValue([{ id: "" }]),
+  },
+}));
+
 describe("[Projects get service] Test case", () => {
   it("Should get all projects", async () => {
     const response = await projects.get();
 
-    expect(response).toStrictEqual([]);
+    expect(response).toStrictEqual([{ id: "" }]);
   });
 });
